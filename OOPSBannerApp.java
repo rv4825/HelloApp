707f4a6ecb6a4bf1dfee8933e67365412a89923b
Rithvik <rv4825@srmist.edu.in>
2026-02-14 17:22:37 +0530

    Add OOPSBannerApp.java source file

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp UC2 - Banner display Application
+ * 
+ * @Rithvik Devloper
+ * @version 2
+ * 
+ */
+
+// extend the program to write OOPS in acsii art format
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println("  *****   " + "  *****   " + "*******   " + "  *****  ");
+        System.out.println(" *     *  " + " *     *  " + "*      *  " + " *     * ");
+        System.out.println("*       * " + "*       * " + "*      *  " + "*        ");
+        System.out.println("*       * " + "*       * " + "*******   " + "  *****  ");
+        System.out.println("*       * " + "*       * " + "*         " + "       * ");
+        System.out.println(" *     *  " + " *     *  " + "*         " + " *     * ");
+        System.out.println("  *****   " + "  *****   " + "*         " + "  *****  ");
+    }
+}
