e07e089f587ffe78e4e04d327d810a30c1c71158
Rithvik <rv4825@srmist.edu.in>
2026-02-26 15:01:28 +0530

    wrote OOPS in ACSII style

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,23 @@
+
 /**
- * OOPSBannerApp UC4 - Banner display Application
+ * OOPSBannerApp UC2 - Banner display Application
  * 
  * @Rithvik Devloper
  * @version 2
  * 
  */
 
-// extend the program to write OOPS using arrays
+// extend the program to write OOPS in acsii art format
 
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
-
+        System.out.println("  *****   " + "  *****   " + "*******   " + "  *****  ");
+        System.out.println(" *     *  " + " *     *  " + "*      *  " + " *     * ");
+        System.out.println("*       * " + "*       * " + "*      *  " + "*        ");
+        System.out.println("*       * " + "*       * " + "*******   " + "  *****  ");
+        System.out.println("*       * " + "*       * " + "*         " + "       * ");
+        System.out.println(" *     *  " + " *     *  " + "*         " + " *     * ");
+        System.out.println("  *****   " + "  *****   " + "*         " + "  *****  ");
     }
 }
