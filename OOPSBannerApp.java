ebb27675a251327805194818a16b70103b0f9905
Rithvik <rv4825@srmist.edu.in>
2026-03-01 19:36:27 +0530

    used static functions to make helper functions for printing each letter

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,21 +9,55 @@
 // extend the program to write OOPS using array inline
 
 public class OOPSBannerApp {
+
+    public static String[] getOpattern() {
+        return new String[] {
+                "  *****   ",
+                " *     *  ",
+                "*       * ",
+                "*       * ",
+                "*       * ",
+                " *     *  ",
+                "  *****   ",
+        };
+    }
+
+    public static String[] getPpattern() {
+        return new String[] {
+
+                "*******   ",
+                "*      *  ",
+                "*      *  ",
+                "*******   ",
+                "*         ",
+                "*         ",
+                "*         ",
+        };
+    }
+
+    public static String[] getSpattern() {
+        return new String[] {
+                "  *****  ",
+                " *     * ",
+                "*        ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  ",
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = {
-                String.join("   ", "  *****   ", "  *****   ", "*******   ", "  *****  "),
-                String.join("   ", " *     *  ", " *     *  ", "*      *  ", " *     * "),
-                String.join("   ", "*       * ", "*       * ", "*      *  ", "*        "),
-                String.join("   ", "*       * ", "*       * ", "*******   ", "  *****  "),
-                String.join("   ", "*       * ", "*       * ", "*         ", "       * "),
-                String.join("   ", " *     *  ", " *     *  ", "*         ", " *     * "),
-                String.join("   ", "  *****   ", "  *****   ", "*         ", "  *****  ")
-        };
+        String[] oPattern = getOpattern();
+        String[] pPattern = getPpattern();
+        String[] sPattern = getSpattern();
 
-        for (String line : lines) {
-            System.out.println(line); // or for(String line : lines)
+        for (int i = 0; i < oPattern.length; i++) {
 
+            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
         }
+
     }
+
 }
