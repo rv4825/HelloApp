526522e126fc9c808197192280e9b9bdc4169e68
Rithvik <rv4825@srmist.edu.in>
2026-03-01 18:02:32 +0530

    used array inline to print the statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,29 @@
-
 /**
- * OOPSBannerApp UC2 - Banner display Application
+ * OOPSBannerApp UC5 - using array inline
  * 
  * @Rithvik Devloper
  * @version 2
  * 
  */
 
-// extend the program to write OOPS in acsii art format
+// extend the program to write OOPS using array inline
 
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println("  *****   " + "  *****   " + "*******   " + "  *****  ");
-        System.out.println(" *     *  " + " *     *  " + "*      *  " + " *     * ");
-        System.out.println("*       * " + "*       * " + "*      *  " + "*        ");
-        System.out.println("*       * " + "*       * " + "*******   " + "  *****  ");
-        System.out.println("*       * " + "*       * " + "*         " + "       * ");
-        System.out.println(" *     *  " + " *     *  " + "*         " + " *     * ");
-        System.out.println("  *****   " + "  *****   " + "*         " + "  *****  ");
+        String[] lines = {
+                String.join("   ", "  *****   ", "  *****   ", "*******   ", "  *****  "),
+                String.join("   ", " *     *  ", " *     *  ", "*      *  ", " *     * "),
+                String.join("   ", "*       * ", "*       * ", "*      *  ", "*        "),
+                String.join("   ", "*       * ", "*       * ", "*******   ", "  *****  "),
+                String.join("   ", "*       * ", "*       * ", "*         ", "       * "),
+                String.join("   ", " *     *  ", " *     *  ", "*         ", " *     * "),
+                String.join("   ", "  *****   ", "  *****   ", "*         ", "  *****  ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line); // or for(String line : lines)
+
+        }
     }
 }
